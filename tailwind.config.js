24dcb23096c6c9714dc085af3b0bba45f7692c05
Pakin <me@pakin.me>
2021-07-12 21:56:41 +0700

    Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: {
     content: [
       './layouts/**/*.html',
